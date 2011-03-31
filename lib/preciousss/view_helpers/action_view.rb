d21660bc542448a7ff4ac2d5cdc10353be0943ed
pavelloz <pavelloz@Gmail.com>
2011-03-31 01:55:06 -0700

    Edited lib/preciousss/view_helpers/action_view.rb via GitHub

--- lib/preciousss/view_helpers/action_view.rb
+++ lib/preciousss/view_helpers/action_view.rb
@@ -3,19 +3,19 @@ module Preciousss
     module ActionView
 
       def body_class
-        qualified_controller_name = controller.controller_path.gsub('/', '-')
-        "#{qualified_controller_name} #{controller.action_name} #{qualified_controller_name}-#{controller.action_name} #{I18n.locale}"
+        qualified_controller_name = controller.controller_path.gsub('/', '_')
+        "#{qualified_controller_name} #{controller.action_name} #{qualified_controller_name}_#{controller.action_name} #{I18n.locale}"
       end
 
       def body_id
-        "#{controller.controller_path.gsub('/', '_')}-body"
+        "#{controller.controller_path.gsub('/', '_')}_body"
       end
 
       def flash_messages
         return if flash.blank?
 
-        content_tag(:div, :class => "flash-messages #{flash.keys.map{|key| "with-#{key}"}.join(' ')}") do
-          flash.map{|key, value| content_tag(:p, content_tag(:span, value), :class => "flash-#{key}")}.join.html_safe
+        content_tag(:div, :class => "flash_messages #{flash.keys.map{|key| "with_#{key}"}.join(' ')}") do
+          flash.map{|key, value| content_tag(:p, content_tag(:span, value), :class => "flash_#{key}")}.join.html_safe
         end
       end
 
